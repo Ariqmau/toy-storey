bc35754018921df86ae87c34e9269555449f3e68
Ariqmau <ariqmalik@gmail.com>
2024-09-22 20:46:01 +0700

    Tugas 4

--- toy_storey/settings.py
+++ toy_storey/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n6qpv^m4@fsiie3(88d$b0$v=ehawx#hbr_0wvt4@(sgi2vk(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ariq-maulana-toystorey.pbp.cs.ui.ac.id"]
 
@@ -127,5 +129,3 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://ariq-maulana-toystorey.pbp.cs.ui.ac.id", "https://ariq-maulana-toystorey.pbp.cs.ui.ac.id"]
-
-# tes
